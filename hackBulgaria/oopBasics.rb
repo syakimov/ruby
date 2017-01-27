914c418d7cf9001f645690f3d95a4c94888ddf1c
syakimov <stefan.yakimov@tradeo.com>
2017-01-27 15:38:09 +0200

    OOP basics

---
+++ hackBulgaria/oopBasics.rb
@@ -0,0 +1,44 @@
+class Dog
+  def initialize(name, owner, is_biting, dog_years)
+    @name = name
+    @owner = owner
+    @is_biting = is_biting
+    @dog_years = dog_years
+  end
+
+  attr_reader :name
+  attr_writer :name
+
+  attr_reader :owner
+  attr_writer :owner
+
+  attr_reader :dog_years
+  attr_writer :dog_years
+
+  def to_human_years
+    puts 'DogTest#test_dog_to_human_years has erorrs'
+    @dog_years * 7
+  end
+
+  def bites?
+    @is_biting
+  end
+
+  def bark
+    'Bark! Bark!'
+  end
+
+  def bark!
+    @is_biting = true
+    'Bark! Bark!'
+  end
+
+  def ==(other)
+    @name == other.name && @owner == other.owner &&
+      @dog_years == other.dog_years
+  end
+
+  def same_owner?(dog)
+    @owner == dog.owner
+  end
+end
