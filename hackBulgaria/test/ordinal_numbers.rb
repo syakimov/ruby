7e7c27c4d6ed67d37d3acc7f447a96684f2a1dc2
syakimov <stefan.yakimov@tradeo.com>
2017-03-01 11:20:43 +0200

    Refactor ordinal numbers

--- hackBulgaria/test/ordinal_numbers.rb
+++ hackBulgaria/test/ordinal_numbers.rb
@@ -1,15 +1,9 @@
-# rubocop disable
-
 def cool_numbers(num)
   return num.to_s + 'th' if num.abs == 11 || num.abs == 12 || num.abs == 13
   case (num % 10).abs
-  when 1
-    num.to_s + 'st'
-  when 2
-    num.to_s + 'nd'
-  when 3
-    num.to_s + 'rd'
-  else
-    num.to_s + 'th'
+  when 1 then num.to_s + 'st'
+  when 2 then num.to_s + 'nd'
+  when 3 then num.to_s + 'rd'
+  else num.to_s + 'th'
   end
 end
