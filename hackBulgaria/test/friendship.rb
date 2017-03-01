d94fcaab396c1d88ce0e3e6248a1514873216c33
syakimov <stefan.yakimov@tradeo.com>
2017-03-01 11:09:43 +0200

    Refactor friendship.rb

--- hackBulgaria/test/friendship.rb
+++ hackBulgaria/test/friendship.rb
@@ -44,33 +44,15 @@ module Friendship
     end
 
     def find(filter)
-      return @friends.select{ |e| e.name == filter[:name]} if filter[:name]
-      return @friends.select{ |e| e.sex == filter[:sex]} if filter[:sex]
-      return @friends.select{ |e| e.age == filter[:age]} if filter[:age]
+      return @friends.select { |e| e.name == filter[:name] } if filter[:name]
+      return @friends.select { |e| e.sex == filter[:sex] } if filter[:sex]
       @friends.select(&filter[:filter])
     end
 
     def unfriend(filter)
-      @friends = @friends.reject{ |e| e.name == filter[:name]} if filter[:name]
-      @friends = @friends.reject{ |e| e.sex == filter[:sex]} if filter[:sex]
-      @friends = @friends.reject{ |e| e.age == filter[:age]} if filter[:age]
-      @friends = @friends.reject(&filter[:filter]) if filter[:filter]
+      @friends.reject! { |e| e.name == filter[:name] } if filter[:name]
+      @friends.reject! { |e| e.sex == filter[:sex] } if filter[:sex]
+      @friends.reject!(&filter[:filter]) if filter[:filter]
     end
   end
 end
-
-friends = Friendship::Database.new
-friends.add_friend('Peter', :male, 28)
-friends.add_friend('Maria', :female, 25)
-friends.add_friend('Denis', :male, 18)
-
-puts friends.map(&:name) #=>  ['Peter', 'Maria', 'Denis']
-
-friends.unfriend(name: 'Denis')
-puts friends.map(&:name) #=> ['Peter', 'Maria']
-
-puts friends.unfriend(sex: :male)
-puts friends.map(&:name) #=> ['Maria']
-
-puts friends.unfriend(filter: ->(friend) { friend.age == 25 })
-puts friends.have_any_friends? #=> false
