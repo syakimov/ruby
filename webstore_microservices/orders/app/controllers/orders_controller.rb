02c5247d2613f947837ac541298f185b3a39a2cc
syakimov <stefan.yakimov@tradeo.com>
2017-06-27 18:09:04 +0300

    Exploit user endpoint

--- webstore_microservices/orders/app/controllers/orders_controller.rb
+++ webstore_microservices/orders/app/controllers/orders_controller.rb
@@ -1,7 +1,24 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.all
+    @orders = Order.all.map do |order|
+      order.merge get_user(order.user_id)
+    end
+  end
+
+  private
+
+  def get_user(id)
+    # get this from coniguration
+    client = client('localhost', '3000')
+    response = client.get(order.user_id.to_s)
+    JSON.parse(response.body).symbolize_keys
+  end
+
+  def client(host, port)
+    Faraday.new(url: "http://#{host}:#{port}/users") do |faraday|
+      faraday.adapter :httpclient
+    end
   end
 
 end
