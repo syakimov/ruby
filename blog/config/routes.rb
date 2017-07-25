3e46afb8faa55eb291ef45fa6ac031b5529587ce
syakimov <stefan.yakimov@tradeo.com>
2017-07-25 13:04:42 +0300

    Rails blog: Hello world

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # http://localhost:3000/welcome/index
+  get 'welcome/index'
+
+  root 'welcome#index'
 end
