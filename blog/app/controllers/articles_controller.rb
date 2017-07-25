7775928de403430d69c973c8b9f79f19fdf8bddd
syakimov <stefan.yakimov@tradeo.com>
2017-07-25 15:14:23 +0300

    Add create article action and show view

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(articles_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  # Rails `strong parameters`
+  # http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  def articles_params
+    params.require(:article).permit(:title, :text)
   end
 end
